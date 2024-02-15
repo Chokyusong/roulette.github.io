3a7583ccb09d37fb283b8d4f6b43808ec6a8cf65
Chokyusong <69471443+Chokyusong@users.noreply.github.com>
2024-02-15 18:26:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const correctPassword = "muse"; // 여기에 원하는 4자리 비밀번호를 설정하세요
+
+function checkPassword() {
+    const inputPassword = document.getElementById('password').value;
+    if(inputPassword === correctPassword) {
+        window.location.href = './index2.html'; // 비밀번호가 올바르면 index2.html로 이동
+    } else {
+        alert('비밀번호가 틀렸습니다. 다시 시도해주세요.');
+    }
+}
